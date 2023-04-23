382823b275e6fac49b437cacce024c5a0ad9731b
Iyad <eyad6823@gmail.com>
2023-04-24 00:32:32 +0300

    add some congrats phares, function for random phares, and make the website reponsive with small screens

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+// vars
+let congrats = ["كل عام وأنتم بخير", "من العايدين السعيدين", "كل سنة وأنت طيب",
+                "أشرف الأعياد", "من السالمين والغانمين", "من العايدين والفايزين",
+                "عيدكم مبارك", "عيد سعيد", "أطيب التهاني والتحايا",
+                "كل عام وأنتم أقرب إلى الجنة وأبعد عن النار", "أعاده الله علينا وعليكم بالخير", "تقبل الله منا ومنكم صالح الأعمال"];
+
+let random = Math.floor(Math.random() * congrats.length);
+
+
 // dom vars
 let pharseContainer = document.querySelector(".congPharse-container");
 let closeButton = document.querySelector("#close");
@@ -9,16 +18,19 @@ button.addEventListener("click", function() {
     pharseContainer.classList.add("open");
     closeButton.classList.add("open");
     pharse.classList.add("open");
+
+    pharse.innerHTML = `<p>${congrats[random]}</p>`;
+
 });
 
 closeButton.addEventListener("click", function() {
     pharseContainer.classList.remove("open");
     closeButton.classList.remove("open");
+    location.reload();
 })
 
-let random = Math.floor(Math.random() * 100);
-console.log(random);
 
-let congrats = ["", "", "",
-                "", "", "",
-                "", "", ""]
+
+
+
+
